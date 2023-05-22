95022bdc1c43f30229110ba223b5ebdf565767cd
RGneo7 <neo007sol@gmail.com>
2023-05-21 21:14:54 -0400

    terminado video 4 - ordenar de forma asc o desc segun lo quiera el cliente - personalizar con iconos referentes

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -23,4 +23,18 @@ class ShowPosts extends Component
         ->get();
         return view('livewire.show-posts', compact('posts'));
     }
+
+    public function order($sort){
+        if ($this->sort == $sort) {
+            if ($this->direction == "desc") {
+                $this->direction="asc";
+            } else {
+                $this->direction="desc";
+            }
+        } else {
+            $this->sort=$sort;
+            $this->direction="asc";
+        }
+        
+    }
 }
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -13,9 +13,39 @@
         <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
             <thead class="bg-gray-50">
                 <tr>
-                    <th scope="col" class="cursor-pointer px-6 py-4 font-medium text-gray-900">Id</th>
-                    <th scope="col" class="cursor-pointer px-6 py-4 font-medium text-gray-900">Title</th>
-                    <th scope="col" class="cursor-pointer px-6 py-4 font-medium text-gray-900">Content</th>
+                    <th scope="col" class="cursor-pointer px-6 py-4 font-medium text-gray-900" wire:click="order('id')">Id
+                        @if ($sort=="id")
+                            @if ($direction=="asc")
+                                <i class="fa-solid fa-arrow-down-1-9 float-right mt-1"></i>
+                            @else
+                                <i class="fa-solid fa-arrow-down-9-1 float-right mt-1"></i>
+                            @endif
+                        @else
+                            <i class="fa-solid fa-sort float-right mt-1"></i>
+                        @endif
+                    </th>
+                    <th scope="col" class="cursor-pointer px-6 py-4 font-medium text-gray-900" wire:click="order('title')">Title 
+                        @if ($sort=="title")
+                            @if ($direction=="asc")
+                                <i class="fa-solid fa-arrow-up-z-a float-right mt-1"></i>
+                            @else
+                                <i class="fa-solid fa-arrow-down-z-a float-right mt-1"></i>
+                            @endif
+                        @else
+                            <i class="fa-solid fa-sort float-right mt-1"></i>
+                        @endif
+                    </th>
+                    <th scope="col" class="cursor-pointer px-6 py-4 font-medium text-gray-900" wire:click="order('content')">Content
+                        @if ($sort=="content")
+                            @if ($direction=="asc")
+                                <i class="fa-solid fa-arrow-up-z-a float-right mt-1"></i>
+                            @else
+                                <i class="fa-solid fa-arrow-down-z-a float-right mt-1"></i>
+                            @endif
+                        @else
+                            <i class="fa-solid fa-sort float-right mt-1"></i>
+                        @endif
+                    </th>
                     <th scope="col" class="px-6 py-4 font-medium text-gray-900"></th>
                 </tr>
             </thead>
