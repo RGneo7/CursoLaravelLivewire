01539c562dca0957a9e4ff693dcaa1aa493ea210
RGneo7 <neo007sol@gmail.com>
2023-05-24 23:37:33 -0400

    terminado video 11 - parametros de accion - reordenamiento de codigo

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -2,17 +2,33 @@
 
 namespace App\Http\Livewire;
 
-use Livewire\Component;
 use App\Models\Post;
+use Livewire\Component;
+//para la carga de imagenes
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Storage;
 
 class ShowPosts extends Component
 {
+    use WithFileUploads;
 
-    public $search;
+    public $search, $post, $image, $identificador;
     public $sort="id";
     public $direction="desc";
+    public $open_edit=false;
 
     protected $listeners=['render' => 'render'];
+
+    public function mount(Post $post){
+        $this->post=$post;
+        //lo siguiente es para definir que le agregue un valor aleatorio cn la intencion de obligar al renderizado a cambiar el valor por defecto del conponente de carga de imagen
+        $this->identificador=rand();
+    }
+
+    protected $rules = [
+        'post.title' => 'required',
+        'post.content' => 'required',
+    ];
 
     public function render()
     {
@@ -37,5 +53,25 @@ class ShowPosts extends Component
             $this->direction="asc";
         }
         
+    }
+
+    public function edit(Post $post){
+        $this->post=$post;
+        $this->open_edit=true;
+    }
+
+    public function update(){
+        $this->validate();
+        if ($this->image) {//si existia una imagen ya en este post
+            Storage::delete([$this->post->image]);//elimino
+
+            $this->post->image = $this->image->store('posts');//y reemplazo por la nueva
+        }
+        $this->post->save();
+        $this->reset(['open_edit', 'image']);
+        //lo siguiente es para definir que le agregue un valor aleatorio cn la intencion de obligar al renderizado a cambiar el valor por defecto del conponente de carga de imagen
+        $this->identificador=rand();
+        // $this->emitTo('show-posts', 'render');
+        $this->emit('alert', 'El post se actualizo satisfactoriamente.');
     }
 }
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -49,18 +49,18 @@
                 </tr>
             </thead>
             <tbody class="divide-y divide-gray-100 border-t border-gray-100">
-                @foreach ($posts as $post)
+                @foreach ($posts as $item)
                     <tr class="hover:bg-gray-50">
                         <th class="flex gap-3 px-6 py-4 font-normal text-gray-900">
-                            <div class="font-medium text-gray-700">{{ $post->id }}</div>
+                            <div class="font-medium text-gray-700">{{ $item->id }}</div>
                         </th>
                         <td class="px-6 py-4">
                             <span
                                 class="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600">
-                                {{ $post->title }}
+                                {{ $item->title }}
                             </span>
                         </td>
-                        <td class="px-6 py-4">{{ $post->content }}</td>
+                        <td class="px-6 py-4">{{ $item->content }}</td>
                         <td class="px-6 py-4">
                             <div class="flex justify-end gap-4">
                                 {{-- <a x-data="{ tooltip: 'Delete' }" href="#">
@@ -70,7 +70,10 @@
                                             d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                     </svg>
                                 </a> --}}
-                                @livewire('edit-post', ['post' => $post], key($post->id))
+                                {{-- @livewire('edit-post', ['post' => $post], key($post->id)) --}}
+                                <a class="btn btn-green" wire:click="edit({{ $item }})">
+                                    <i class="fas fa-edit"></i>
+                                </a>
                             </div>
                         </td>
                     </tr>
@@ -83,4 +86,38 @@
             </div>
         @endif
     </x-table>
+
+    <x-dialog-modal wire:model="open_edit">
+        <x-slot name="title">
+            Editar el post {{ $post->title }}
+        </x-slot>
+        <x-slot name="content">
+            <div wire:loading wire:targe="image" class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Cargando imagen!</strong>
+                <span class="block sm:inline">Espere un momento, procesando imagen.</span>
+              </div>
+
+            @if ($image)
+                <img src="{{ $image->temporaryUrl() }}" alt="">
+            @else
+              <img src="{{ Storage::url($post->image) }}" alt="">
+            @endif
+            <div class="mb-4">
+                <x-label value="Titulo"></x-label>
+                <x-input wire:model="post.title" type="text" class="w-full"></x-input>
+            </div>
+            <div class="mb-4">
+                <x-label value="Contenido"></x-label>
+                <textarea wire:model="post.content" name="" id="" rows="6" class="form-control w-full"></textarea>
+            </div>
+            <div class="mb-4">
+                <input type="file" name="" id="{{ $identificador }}" wire:model="image">
+                <x-input-error for="image"></x-input-error>
+            </div>
+        </x-slot>
+        <x-slot name="footer">
+            <x-secondary-button wire:click="$set('open_edit', false)">Cancelar</x-secondary-button>
+            <x-danger-button wire:click="update" wire:loading.attr="disabled" wire:target="save, image" class="disabled:opacity-25">Actualizar</x-danger-button>
+        </x-slot>
+    </x-dialog-modal>
 </div>
